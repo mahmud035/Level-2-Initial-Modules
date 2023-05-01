ab82d27178d54be886bc677b108d1eeda08dd6f3
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-01 07:50:50 +0600

    2-3 :Array and Tuples in typescript

---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/array-tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+// Array
+const names = ['abul', 'kabul', 'babul'];
+const rollNumbers = [44, 55, 66];
+const mixedTypesArray = ['abul', 44, true];
+// Tuples
+const user = [112, 'John'];
---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/array-tuples.ts
@@ -0,0 +1,8 @@
+// Array
+const names: string[] = ['abul', 'kabul', 'babul'];
+const rollNumbers: number[] = [44, 55, 66];
+
+const mixedTypesArray: (string | number | boolean)[] = ['abul', 44, true];
+
+// Tuples
+const user: [number, string] = [112, 'John'];
