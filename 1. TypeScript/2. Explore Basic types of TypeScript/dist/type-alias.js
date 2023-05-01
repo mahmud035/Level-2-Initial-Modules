3fdcd779393bca4ede90c81c3aba0bbce7f9f47b
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-01 10:43:00 +0600

    2-7: Type alias and optional types

---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/type-alias.js
@@ -0,0 +1,12 @@
+"use strict";
+const crush1 = {
+    name: 'Moina Pakhi',
+    age: 22,
+    profession: 'Web Developer',
+    address: 'Uganda',
+};
+const crush2 = {
+    name: 'Tia Pakhi',
+    profession: 'Web Designer',
+    address: 'Uganda',
+};
---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/type-alias.ts
@@ -0,0 +1,20 @@
+//* type alias
+type Crush = {
+  name: string;
+  age?: number; // optional parameter
+  profession: string;
+  address: string;
+};
+
+const crush1: Crush = {
+  name: 'Moina Pakhi',
+  age: 22,
+  profession: 'Web Developer',
+  address: 'Uganda',
+};
+
+const crush2: Crush = {
+  name: 'Tia Pakhi',
+  profession: 'Web Designer',
+  address: 'Uganda',
+};
