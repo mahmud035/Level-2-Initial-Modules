ac4c0cbb9769a6547b875e7ed40dfafda78671fa
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-01 07:29:36 +0600

    2-2 Primitive types in typescript

---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/primitive-types.js
@@ -0,0 +1,7 @@
+"use strict";
+let course = 'Next Level Web Development';
+course = 'Next Level Development';
+// course = 2321;  // Error Here
+let rollNumber = 123;
+rollNumber = 666;
+const isAdmin = true;
---
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/primitive-types.ts
@@ -0,0 +1,9 @@
+let course: string = 'Next Level Web Development';
+
+course = 'Next Level Development';
+// course = 2321;  // Error Here
+
+let rollNumber: number = 123;
+rollNumber = 666;
+
+const isAdmin: boolean = true;
