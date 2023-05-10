879bb22f369f0bc7871c6bd8e84157586be1c947
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-10 10:56:22 +0600

    More utility types from documentation

--- 1. TypeScript/5.5 Bonus Module/src/utility.ts
+++ 1. TypeScript/5.5 Bonus Module/src/utility.ts
@@ -1,4 +1,4 @@
-//* Pick
+//* Pick<Type, Keys>
 interface Person {
   name: string;
   email?: string;
@@ -8,14 +8,14 @@ interface Person {
 type Email = Pick<Person, 'email'>;
 type Contact = Pick<Person, 'contactNo' | 'email'>;
 
-//* Omit
+//* Omit<Type, Keys>
 type Name = Omit<Person, 'email' | 'contactNo'>;
 
-//* Partial
+//* Partial<Type>
 // To make all the properties Optional types => use Partial type
 type Optional = Partial<Person>;
 
-//* Required
+//* Required<Type>
 // To make all the properties Required => use Required type
 type RequiredProps = Required<Person>;
 
@@ -28,7 +28,7 @@ const person: Readonly<Person> = {
 
 // person.name = 'Alex'; // Error Here
 
-//* Record Type
+//* Record<Keys, Type>
 // type MyObj = {
 //   a: string;
 //   b: string;
@@ -50,3 +50,24 @@ const obj: MyObj = {
   c: '3',
   // d: '4', // Error Here (for first MyObj using Record)
 };
+
+//* ================== Documentation ======================
+//* Awaited<Type>
+type A = Awaited<Promise<string>>;
+
+type B = Awaited<Promise<Promise<number>>>;
+
+type C = Awaited<boolean | Promise<number>>;
+
+//* Exclude<UnionType, ExcludedMembers>
+type T0 = Exclude<'a' | 'b' | 'c', 'a'>;
+
+type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>;
+
+type T2 = Exclude<string | number | (() => void), Function>;
+
+//* NonNullable<Type>
+// Constructs a type by excluding null and undefined from Type.
+type T3 = NonNullable<string | number | undefined>;
+
+type T4 = NonNullable<string[] | null | undefined>;
