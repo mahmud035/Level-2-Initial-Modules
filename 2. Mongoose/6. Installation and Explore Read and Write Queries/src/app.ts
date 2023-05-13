0dbbcd33b0e352b639a022b9cc59552892339d71
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-13 12:06:10 +0600

    6-4: How To relate Interface and schema

--- 2. Mongoose/6. Installation and Explore Read and Write Queries/src/app.ts
+++ 2. Mongoose/6. Installation and Explore Read and Write Queries/src/app.ts
@@ -1,15 +1,86 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { Schema, model } from 'mongoose';
 
 const app: Application = express();
 
 //* Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!');
-  next();
+app.post('/', (req: Request, res: Response, next: NextFunction) => {
+  // NOTE: Inserting a test data into mongodb
+  /**
+   * Step1: Create an Interface
+   * Step2: Create a Schema
+   * Step3: Create a Model
+   * Step4: Connect to MongoDB (Database Query)
+   * */
+
+  // Step1: Create an Interface
+  interface IUser {
+    id: string;
+    role: 'student';
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: 'male' | 'female';
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // Step2: Create a Schema Using The Interface
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: { type: String, required: true },
+      middleName: { type: String },
+      lastName: { type: String, required: true },
+    },
+    dateOfBirth: { type: String },
+    gender: { type: String, enum: ['male', 'female'] },
+    email: { type: String },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
+
+  // Step3: Create a Model
+  const User = model<IUser>('User', userSchema);
+
+  // Step4: Database Query on Model
+  const createUser = async () => {
+    const user = new User({
+      id: '111',
+      role: 'student',
+      password: '123abc',
+      name: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      dateOfBirth: 'March 1990',
+      email: 'john@gmail.com',
+      contactNo: '019999999',
+      emergencyContactNo: '017777',
+      presentAddress: 'Gopalganj',
+      permanentAddress: 'Grave',
+    });
+
+    await user.save();
+  };
+
+  createUser();
 });
 
 export default app;
