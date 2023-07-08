455c6e773b8f8847e3afd3e69f4c819f110a75e1
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-08 08:59:53 +0600

    create basic productSlice and connected with store

---
+++ 4. Be A Redux Reaper/21. Hands on State Management and CRUD Operation/technet-react-redux-practice/src/redux/products/productSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface IProduct {
+  status: boolean;
+  priceRange: number;
+}
+
+const initialState: IProduct = {
+  status: false,
+  priceRange: 150,
+};
+
+const productSlice = createSlice({
+  name: 'product',
+  initialState,
+  reducers: {},
+});
+
+export default productSlice.reducer;
--- 4. Be A Redux Reaper/21. Hands on State Management and CRUD Operation/technet-react-redux-practice/src/redux/store.ts
+++ 4. Be A Redux Reaper/21. Hands on State Management and CRUD Operation/technet-react-redux-practice/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './features/cart/cartSlice';
+import productSlice from './products/productSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    product: productSlice,
   },
 });
 
