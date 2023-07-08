0e2398bd8ca5ae381539c1167129882eff28bf55
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-08 09:35:47 +0600

    get all products data using RTK Query

--- 4. Be A Redux Reaper/21. Hands on State Management and CRUD Operation/technet-react-redux-practice/src/pages/Products.tsx
+++ 4. Be A Redux Reaper/21. Hands on State Management and CRUD Operation/technet-react-redux-practice/src/pages/Products.tsx
@@ -2,18 +2,17 @@ import ProductCard from '@/components/ProductCard';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
+import { useGetProductsQuery } from '@/redux/api/apiSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { setPriceRange, toggleState } from '@/redux/products/productSlice';
 import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('./data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  const { data, error, isLoading } = useGetProductsQuery(undefined);
+
+  console.log(isLoading);
+  console.log(data);
+  console.log(error);
 
   const { status, priceRange } = useAppSelector((state) => state.product);
   const dispatch = useAppDispatch();
@@ -25,13 +24,16 @@ export default function Products() {
 
   let productsData;
 
-  // filter product based on status and priceRange
+  //* filter product based on status and priceRange
   if (status) {
-    productsData = data.filter(
-      (item) => item.status === true && item.price < priceRange
+    productsData = data?.data?.filter(
+      (item: { status: boolean; price: number }) =>
+        item.status === true && item.price < priceRange
     );
   } else if (priceRange > 0) {
-    productsData = data.filter((item) => item.price < priceRange);
+    productsData = data?.data?.filter(
+      (item: { price: number }) => item.price < priceRange
+    );
   } else {
     productsData = data;
   }
@@ -64,7 +66,7 @@ export default function Products() {
         </div>
       </div>
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {productsData?.map((product) => (
+        {productsData?.map((product: IProduct) => (
           <ProductCard product={product} />
         ))}
       </div>
