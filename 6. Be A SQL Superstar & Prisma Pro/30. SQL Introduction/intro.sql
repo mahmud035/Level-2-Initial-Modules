7bfcb73788ec38e365ae358288a9b96133dfbd74
mahmud035 <mahamudulhasan4148@gmail.com>
2023-08-07 19:15:18 +0600

    MODULE VIDEO CODE: 30-6: Create, Update, Delete A Table

--- 6. Be A SQL Superstar & Prisma Pro/30. SQL Introduction/intro.sql
+++ 6. Be A SQL Superstar & Prisma Pro/30. SQL Introduction/intro.sql
@@ -1,5 +1,25 @@
+-- Active: 1691400077453@@127.0.0.1@5432@test1
+
 -- CREATE DATABASE test2;
 
 -- ALTER DATABASE test2 RENAME TO test3;
 
 -- DROP DATABASE test3;
+
+-- CREATE TABLE
+
+--     IF NOT EXISTS student(
+
+--         student_id INT,
+
+--         first_name VARCHAR(50),
+
+--         last_name VARCHAR(50),
+
+--         cgpa NUMERIC(1, 2)
+
+--     );
+
+-- ALTER TABLE student RENAME TO new_student;
+
+-- DROP TABLE IF EXISTS new_student;
