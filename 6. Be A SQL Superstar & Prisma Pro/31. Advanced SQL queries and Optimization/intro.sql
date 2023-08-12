76c1174ffdd7f0705f7c28609295a58326f270ce
mahmud035 <mahamudulhasan4148@gmail.com>
2023-08-12 16:19:23 +0600

    31-2: Updation And Deletion

--- 6. Be A SQL Superstar & Prisma Pro/31. Advanced SQL queries and Optimization/intro.sql
+++ 6. Be A SQL Superstar & Prisma Pro/31. Advanced SQL queries and Optimization/intro.sql
@@ -1,9 +1,7 @@
 -- CREATE DATABASE test2;
 
--- Department Table
-
+--* Department Table
 -- Each department has many employees.
-
 CREATE TABLE
     IF NOT EXISTS Department (
         Department_Id SERIAL PRIMARY KEY,
@@ -18,16 +16,16 @@ DELETE FROM Department WHERE Department_Id = 1 ;
 
 SELECT * FROM Department;
 
--- Employee Table
-
+--* Employee Table
 -- Each employee belongs to a different
-
 CREATE TABLE
     IF NOT EXISTS Employee (
         Employee_Id SERIAL PRIMARY KEY,
         Employee_Name VARCHAR(255) NOT NULL,
         Department_Id INT,
-        CONSTRAINT fk_constraint_dept FOREIGN KEY (Department_Id) REFERENCES Department (Department_Id)
+        CONSTRAINT fk_constraint_dept 
+        FOREIGN KEY (Department_Id) 
+        REFERENCES Department (Department_Id)
     );
 
 DROP TABLE IF EXISTS Employee;
@@ -37,3 +35,68 @@ INSERT INTO Employee VALUES(1, 'John', 1) ;
 DELETE FROM Employee WHERE Employee_Id = 1;
 
 SELECT * FROM Employee;
+
+-- WARNING: Be careful when updating and deleting. Always specify the WHERE Condition.
+
+--* 31-2: Updation And Deletion
+create table courses (
+  course_id SERIAL PRIMARY KEY,
+  course_name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  published_date DATE
+);
+
+DROP TABLE IF EXISTS courses;
+
+SELECT * FROM courses;
+
+-- Date -> YYYY-MM-DD
+INSERT INTO 
+  courses (course_name, description, published_date)
+VALUES
+  (
+  'Postgres for Developer', 
+  'A complete Postgres for developers', 
+  '2020-07-13'
+  ),
+  (
+  'Postgres Administration', 
+  'A Postgres guide for DBA', 
+  NULL
+  ),
+  ('Postgres High Performance', NULL, NULL),
+  (
+  'Postgres Bootcamp', 
+  'Learn Postgres via Bootcamp', 
+  '201-07-11'
+  ),
+  (
+  'Mastering Postgres', 
+  'Mastering Postgres in 21 Days', 
+  '2012-06-30'
+  );
+
+TRUNCATE TABLE courses;
+
+--* Update database table row
+-- UPDATE table_name
+-- SET column1 = value1,
+--     column2 = value2,
+--     ...
+-- WHERE condition;
+
+UPDATE courses
+SET course_name= 'Postgres',
+    description = 'Dummy text'
+WHERE course_Id > 1 AND course_id < 5 ;
+
+SELECT * FROM courses;
+
+--* Delete database table row
+-- DELETE FROM table_name
+-- WHERE condition;
+
+DELETE FROM courses
+WHERE course_id = 1;
+
+SELECT * FROM courses;
