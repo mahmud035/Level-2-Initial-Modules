adcecd711e68dd9765bef45143a6a038516864ce
mahmud035 <mahamudulhasan4148@gmail.com>
2023-08-18 12:04:01 +0600

    32-4: Write Your First Query With Prisma Client ( Create And Read )

---
+++ 6. Be A SQL Superstar & Prisma Pro/32. Prisma/hello-prisma/index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const postUser = await prisma.user.create({
+    data: {
+      email: 'fahim@ph.com',
+      name: 'fahim',
+    },
+  });
+
+  const getAllUsers = await prisma.user.findMany();
+  console.log(getAllUsers);
+}
+
+main();
