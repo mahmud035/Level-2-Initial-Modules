7d75c003533e869062fde8c8f842e3868e227b69
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-10-14 06:35:44 +0600

    26-6-2 : A deep dive into Next.Js App Router

--- 5. Be A NEXT.js Ninja/26-6. Next.js App Router/nextjs-app-router/src/app/layout.js
+++ 5. Be A NEXT.js Ninja/26-6. Next.js App Router/nextjs-app-router/src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body>
         <header>
           <p>This is header</p>
--- 5. Be A NEXT.js Ninja/26-6. Next.js App Router/nextjs-app-router/src/app/page.js
+++ 5. Be A NEXT.js Ninja/26-6. Next.js App Router/nextjs-app-router/src/app/page.js
@@ -1,7 +1,12 @@
 const HomePage = () => {
+  console.log('From Server');
+
   return (
     <div>
       <h1 className="text-2xl text-teal-500">This is home page</h1>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
     </div>
   );
 };
