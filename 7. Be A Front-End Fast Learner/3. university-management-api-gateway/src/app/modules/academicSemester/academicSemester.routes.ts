d7202b27adca5521aa94d7c43a6a774a4ec01255
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-10-23 15:27:24 +0600

    46-1 Creating an Academic Semester through the API Gateway

--- 7. Be A Front-End Fast Learner/3. university-management-api-gateway/src/app/modules/academicSemester/academicSemester.routes.ts
+++ 7. Be A Front-End Fast Learner/3. university-management-api-gateway/src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
-import validateRequest from '../../middlewares/validateRequest';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validations';
 
@@ -11,23 +11,23 @@ router.get('/', AcademicSemesterController.getAllFromDB);
 router.get('/:id', AcademicSemesterController.getByIdFromDB);
 
 router.post(
-    '/',
-    validateRequest(AcademicSemesterValidation.create),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.insertIntoDB
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.insertIntoDB
 );
 
 router.patch(
-    '/:id',
-    validateRequest(AcademicSemesterValidation.update),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.updateOneIntoDB
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneIntoDB
 );
 
 router.delete(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.deleteByIdFromDB
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
 );
 
 export const academicSemesterRoutes = router;
