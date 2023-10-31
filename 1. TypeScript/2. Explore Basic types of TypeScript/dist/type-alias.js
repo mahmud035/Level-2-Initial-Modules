81d0105375bde0402d385d9e77226ced363d1eac
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-10-31 21:17:59 +0600

    2-7: Type alias and optional types

--- 1. TypeScript/2. Explore Basic types of TypeScript/dist/type-alias.js
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/type-alias.js
@@ -10,3 +10,14 @@ const crush2 = {
     profession: 'Web Designer',
     address: 'Uganda',
 };
+const isCrushMarried = false;
+const courseName = 'Next Level Web Development';
+const calculate = (num1, // 10
+num2, // 20
+operation // (x, y) => x + y) || (10, 20) => 10 + 20
+) => {
+    return operation(num1, num2);
+};
+console.log(calculate(10, 20, (x, y) => x + y));
+console.log(calculate(20, 10, (x, y) => x - y));
+console.log(calculate(20, 10, (x, y) => x * y));
--- 1. TypeScript/2. Explore Basic types of TypeScript/src/type-alias.ts
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/type-alias.ts
@@ -1,4 +1,5 @@
-//* type alias
+//* 1. Basic Type alias
+// IMPORTANT:
 type Crush = {
   name: string;
   age?: number; // optional parameter
@@ -17,4 +18,53 @@ const crush2: Crush = {
   name: 'Tia Pakhi',
   profession: 'Web Designer',
   address: 'Uganda',
+};
+
+type CrushMarriedType = boolean;
+const isCrushMarried: CrushMarriedType = false;
+
+type CourseNameType = string;
+const courseName: CourseNameType = 'Next Level Web Development';
+
+//* 2. Union Type Alias (combining multiple types)
+type Result = number | string;
+
+//* 3. Intersection Type Alias (combining multiple types into one)
+type Person2 = {
+  name: string;
+};
+
+type Address = {
+  city: string;
+};
+
+type Contact = Person2 & Address;
+
+// IMPORTANT:
+//* 4. Mapped Type Alias (transforming existing types)
+type Optional<T> = {
+  [P in keyof T]?: T[P];
+};
+
+// IMPORTANT:
+//* 5. Function Type Alias (describing function signatures)
+type OperationType = (x: number, y: number) => number;
+
+const calculate = (
+  num1: number, // 10
+  num2: number, // 20
+  operation: OperationType // (x, y) => x + y) || (10, 20) => 10 + 20
+): number => {
+  return operation(num1, num2);
+};
+
+console.log(calculate(10, 20, (x, y) => x + y));
+console.log(calculate(20, 10, (x, y) => x - y));
+console.log(calculate(20, 10, (x, y) => x * y));
+
+// IMPORTANT:
+//* 6. Type Alias for Generics (creating reusable type templates)
+type Pair<T, U> = {
+  first: T;
+  second: U;
 };
