6b43aa74278f3b11852657be0ca598f2ae50e8f8
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-11-01 05:59:33 +0600

    2-8: Union, Intersection and Enum Types

--- 1. TypeScript/2. Explore Basic types of TypeScript/dist/union-intersection-enum-types.js
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/union-intersection-enum-types.js
@@ -11,10 +11,47 @@ const seniorDeveloper = {
     leadershipExperience: 1,
     level: 'junior',
 };
-// NOTE: Using enum is NOT RECOMMENDED
+// =================>
+//* Union Types
+// Defining a variable with a union type
+let myVar;
+myVar = 42; // Valid, as it's a number
+myVar = 'Hello'; // Valid, as it's a string
+// myVar = true;  // Invalid, as it's not a number or a string
+// Function parameter with a union type
+function printValue(value) {
+    console.log(value);
+}
+printValue(123); // Valid
+printValue('Hello'); // Valid
+function processStatus(status) {
+    // Logic for handling different status values
+}
+processStatus('success'); // Valid
+processStatus('error'); // Valid
+processStatus('pending'); // Valid
+const john = {
+    name: 'John',
+    age: 30,
+    employeeId: 12345,
+    jobTitle: 'Developer',
+};
+//* Enum Types
 var Level;
 (function (Level) {
-    Level[Level["junior"] = 0] = "junior";
-    Level[Level["mid"] = 1] = "mid";
-    Level[Level["senior"] = 2] = "senior";
+    Level["junior"] = "junior";
+    Level["mid"] = "mid";
+    Level["senior"] = "senior";
 })(Level || (Level = {}));
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+var BooleanLikeHeterogeneousEnum;
+(function (BooleanLikeHeterogeneousEnum) {
+    BooleanLikeHeterogeneousEnum[BooleanLikeHeterogeneousEnum["No"] = 0] = "No";
+    BooleanLikeHeterogeneousEnum["Yes"] = "YES";
+})(BooleanLikeHeterogeneousEnum || (BooleanLikeHeterogeneousEnum = {}));
--- 1. TypeScript/2. Explore Basic types of TypeScript/src/union-intersection-enum-types.ts
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/union-intersection-enum-types.ts
@@ -1,4 +1,4 @@
-//* Union types
+//* Union and Intersection Type (module video)
 type NoobDeveloper = {
   name: string;
 };
@@ -33,9 +33,71 @@ const seniorDeveloper: NextLevelDeveloper = {
   level: 'junior',
 };
 
-// NOTE: Using enum is NOT RECOMMENDED
+// =================>
+
+//* Union Types
+// Defining a variable with a union type
+let myVar: number | string;
+myVar = 42; // Valid, as it's a number
+myVar = 'Hello'; // Valid, as it's a string
+// myVar = true;  // Invalid, as it's not a number or a string
+
+// Function parameter with a union type
+function printValue(value: number | string) {
+  console.log(value);
+}
+
+printValue(123); // Valid
+printValue('Hello'); // Valid
+// printValue(true);  // Invalid
+
+// Union types with other types
+type Status2 = 'success' | 'error' | 'pending';
+
+function processStatus(status: Status2) {
+  // Logic for handling different status values
+}
+
+processStatus('success'); // Valid
+processStatus('error'); // Valid
+processStatus('pending'); // Valid
+// processStatus("other");   // Invalid, as it's not one of the specified values
+
+//* Intersection Types
+type Person3 = {
+  name: string;
+  age: number;
+};
+
+type Employee = {
+  employeeId: number;
+  jobTitle: string;
+};
+
+type PersonEmployee = Person3 & Employee;
+
+const john: PersonEmployee = {
+  name: 'John',
+  age: 30,
+  employeeId: 12345,
+  jobTitle: 'Developer',
+};
+
+//* Enum Types
 enum Level {
-  junior,
-  mid,
-  senior,
+  junior = 'junior',
+  mid = 'mid',
+  senior = 'senior',
+}
+
+enum Direction {
+  Up = 'UP',
+  Down = 'DOWN',
+  Left = 'LEFT',
+  Right = 'RIGHT',
+}
+
+enum BooleanLikeHeterogeneousEnum {
+  No = 0,
+  Yes = 'YES',
 }
