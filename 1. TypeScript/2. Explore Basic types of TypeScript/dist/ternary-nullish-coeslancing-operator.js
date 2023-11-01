b4553f4051d5e10786818634c903c217e266277c
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-11-01 07:04:19 +0600

    2-10: Ternary Operator , Nullish Coeslancing Operator, Module Summary

--- 1. TypeScript/2. Explore Basic types of TypeScript/dist/ternary-nullish-coeslancing-operator.js
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/ternary-nullish-coeslancing-operator.js
@@ -1,15 +1,21 @@
 "use strict";
 var _a;
-//* ternary operator
+//* Ternary Operator
 const age = 20;
 // if (age > 18) {
 //   console.log('Adult');
 // } else {
 //   console.log('Minor');
 // }
+// NOTE: Syntax: condition ? trueValue : falseValue;
 const isAdult = age >= 18 ? 'Yes' : 'No';
 //* Nullish Coeslancing Operator
 /* null and undefined */
+/**
+ * IMPORTANT: - এটাকে double question mark (??) দিয়ে লিখতে হয়। (??) এর বামপাশে যদি `null / undefined` থাকে, তাহলে (??) এর ডানপাশের code execute হবে।
+ *  আর যদি (??) এর বামপাশে `null / undefined` ছাড়া অন্য কোন value থাকে, তাহলে ডানপাশের code execute হবে না। তখন বামপাশের ঐ value টা ই variable এর value হিসেবে বসবে।
+ */
+// Ex-1
 const isAuthenticatedUser = null; // userName will be Guest
 // const isAuthenticatedUser = undefined; // userName will be Guest
 // const isAuthenticatedUser = 'John'; // userName will be John
@@ -24,5 +30,10 @@ const human1 = {
         // home: 'Gopalganj',
     },
 };
-const home = (_a = human1.address.home) !== null && _a !== void 0 ? _a : 'No Home'; // default
-console.log({ home });
+const home = (_a = human1.address.home) !== null && _a !== void 0 ? _a : 'No Home';
+console.log(home); // Output: "No Home"
+// Ex-3
+const someValue = null;
+const defaultValue = 'This is a default value';
+const result = someValue !== null && someValue !== void 0 ? someValue : defaultValue;
+console.log(result); // Output: "This is a default value"
--- 1. TypeScript/2. Explore Basic types of TypeScript/src/ternary-nullish-coeslancing-operator.ts
+++ 1. TypeScript/2. Explore Basic types of TypeScript/src/ternary-nullish-coeslancing-operator.ts
@@ -1,4 +1,4 @@
-//* ternary operator
+//* Ternary Operator
 const age: number = 20;
 
 // if (age > 18) {
@@ -7,11 +7,19 @@ const age: number = 20;
 //   console.log('Minor');
 // }
 
+// NOTE: Syntax: condition ? trueValue : falseValue;
+
 const isAdult = age >= 18 ? 'Yes' : 'No';
 
 //* Nullish Coeslancing Operator
 /* null and undefined */
 
+/**
+ * IMPORTANT: - এটাকে double question mark (??) দিয়ে লিখতে হয়। (??) এর বামপাশে যদি `null / undefined` থাকে, তাহলে (??) এর ডানপাশের code execute হবে।
+ *  আর যদি (??) এর বামপাশে `null / undefined` ছাড়া অন্য কোন value থাকে, তাহলে ডানপাশের code execute হবে না। তখন বামপাশের ঐ value টা ই variable এর value হিসেবে বসবে।
+ */
+
+// Ex-1
 const isAuthenticatedUser = null; // userName will be Guest
 // const isAuthenticatedUser = undefined; // userName will be Guest
 // const isAuthenticatedUser = 'John'; // userName will be John
@@ -19,7 +27,7 @@ const isAuthenticatedUser = null; // userName will be Guest
 const userName = isAuthenticatedUser ?? 'Guest';
 console.log(userName);
 
-// Another Use Case:
+// Ex-2:
 type Human = {
   name: string;
   age: number;
@@ -40,5 +48,12 @@ const human1: Human = {
   },
 };
 
-const home = human1.address.home ?? 'No Home'; // default
-console.log({ home });
+const home = human1.address.home ?? 'No Home';
+console.log(home); // Output: "No Home"
+
+// Ex-3
+const someValue = null;
+const defaultValue = 'This is a default value';
+
+const result = someValue ?? defaultValue;
+console.log(result); // Output: "This is a default value"
