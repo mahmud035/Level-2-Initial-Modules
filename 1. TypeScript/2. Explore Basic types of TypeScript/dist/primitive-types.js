9d7a96ee1a07d657b87386ac33f146963053252e
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-11-02 06:37:19 +0600

    MODIFY: 2-4 Basic data types

--- 1. TypeScript/2. Explore Basic types of TypeScript/dist/primitive-types.js
+++ 1. TypeScript/2. Explore Basic types of TypeScript/dist/primitive-types.js
@@ -12,3 +12,22 @@ let rollNumber = 123;
 let isAdmin = true;
 let myName;
 myName = 'Mahmudul Hasan Pavel';
+let noValue = null;
+let undefinedValue = undefined;
+// NOTE: TypeScript Provided Types:
+/**
+ * Array,
+ * Object,
+ * Tuple,
+ * void,
+ * any,
+ * enum
+ */
+let numbers = [1, 2, 3, 4, 5];
+let fruits = ['apple', 'banana', 'cherry'];
+let person5 = { name: 'Alice', age: 30 };
+let employee = ['John', 35];
+function logMessage(message) {
+    console.log(message);
+}
+let dynamicValue = 'This can be any type';
