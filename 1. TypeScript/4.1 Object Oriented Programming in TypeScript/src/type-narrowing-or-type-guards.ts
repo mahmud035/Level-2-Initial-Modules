ca483108d4a11a5c2e2b80f5daad89ae6a4e634c
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-11-23 18:16:10 +0600

    3-3: Type guard using typeof & in

--- 1. TypeScript/4.1 Object Oriented Programming in TypeScript/src/type-narrowing-or-type-guards.ts
+++ 1. TypeScript/4.1 Object Oriented Programming in TypeScript/src/type-narrowing-or-type-guards.ts
@@ -1,108 +1,110 @@
-//* keyof guard
+{
+  //* keyof guard
 
-type Alphanumeric = string | number;
+  type Alphanumeric = string | number;
 
-function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
-  if (typeof param1 === 'number' && typeof param2 === 'number') {
-    return param1 + param2;
-  } else {
-    return param1.toString() + param2.toString();
-  }
-}
-
-// console.log(add(2, 3));
-// console.log(add('2', '3'));
-
-//* in guard
-
-type NormalUserType = { name: string };
-type AdminUserType = { name: string; role: 'admin' };
-
-function getUser(user: NormalUserType | AdminUserType) {
-  if ('role' in user) {
-    return `I am an admin and my role is ${user.role}`;
-  } else {
-    return `I am a normal user`;
-  }
-}
-
-const normalUser1: NormalUserType = { name: 'Mr. X' };
-const adminUser1: AdminUserType = { name: 'Mr. Y', role: 'admin' };
-
-// console.log(getUser(normalUser1));
-// console.log(getUser(adminUser1));
-
-//* instanceof guard
-/**
- * NOTE: An instance is a real object that we can use which was created from a class.
- * */
-
-class Animal2 {
-  constructor(public name: string, public species: string) {}
-
-  makeSound() {
-    console.log(`I am making sound`);
-  }
-}
-
-class Dog extends Animal2 {
-  constructor(name: string, species: string) {
-    super(name, species);
+  function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
+    if (typeof param1 === 'number' && typeof param2 === 'number') {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
   }
 
-  makeBark() {
-    console.log(`I am barking`);
+  // console.log(add(2, 3));
+  // console.log(add('2', '3'));
+
+  //* in guard
+
+  type NormalUserType = { name: string };
+  type AdminUserType = { name: string; role: 'admin' };
+
+  function getUser(user: NormalUserType | AdminUserType) {
+    if ('role' in user) {
+      return `I am an admin and my role is ${user.role}`;
+    } else {
+      return `I am a normal user`;
+    }
   }
-}
 
-class Cat extends Animal2 {
-  constructor(name: string, species: string) {
-    super(name, species);
+  const normalUser1: NormalUserType = { name: 'Mr. X' };
+  const adminUser1: AdminUserType = { name: 'Mr. Y', role: 'admin' };
+
+  // console.log(getUser(normalUser1));
+  // console.log(getUser(adminUser1));
+
+  //* instanceof guard
+  /**
+   * NOTE: An instance is a real object that we can use which was created from a class.
+   * */
+
+  class Animal2 {
+    constructor(public name: string, public species: string) {}
+
+    makeSound() {
+      console.log(`I am making sound`);
+    }
   }
 
-  makeMeaw() {
-    console.log(`I am meawing`);
+  class Dog extends Animal2 {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+
+    makeBark() {
+      console.log(`I am barking`);
+    }
   }
-}
 
-// instanceof => Dog
-const animal1 = new Dog('German Bhai', 'dog');
+  class Cat extends Animal2 {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
 
-// instanceof => Cat
-const animal2 = new Cat('Persian Bhai', 'cat');
-
-function getAnimal(animal: Animal2) {
-  if (animal instanceof Dog) {
-    animal.makeBark();
-  } else if (animal instanceof Cat) {
-    animal.makeMeaw();
-  } else {
-    animal.makeSound();
+    makeMeaw() {
+      console.log(`I am meawing`);
+    }
   }
-}
 
-// getAnimal(animal1);
-// getAnimal(animal2);
+  // instanceof => Dog
+  const animal1 = new Dog('German Bhai', 'dog');
 
-//* Using type predicates
+  // instanceof => Cat
+  const animal2 = new Cat('Persian Bhai', 'cat');
 
-function isDog(animal: Animal2): animal is Dog {
-  return animal instanceof Dog;
-}
-
-function isCat(animal: Animal2): animal is Cat {
-  return animal instanceof Cat;
-}
-
-function getAnimal2(animal: Animal2) {
-  if (isDog(animal)) {
-    animal.makeBark();
-  } else if (isCat(animal)) {
-    animal.makeMeaw();
-  } else {
-    animal.makeSound();
+  function getAnimal(animal: Animal2) {
+    if (animal instanceof Dog) {
+      animal.makeBark();
+    } else if (animal instanceof Cat) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
   }
-}
 
-getAnimal2(animal1);
-getAnimal2(animal2);
+  // getAnimal(animal1);
+  // getAnimal(animal2);
+
+  //* Using type predicates
+
+  function isDog(animal: Animal2): animal is Dog {
+    return animal instanceof Dog;
+  }
+
+  function isCat(animal: Animal2): animal is Cat {
+    return animal instanceof Cat;
+  }
+
+  function getAnimal2(animal: Animal2) {
+    if (isDog(animal)) {
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
+  }
+
+  getAnimal2(animal1);
+  getAnimal2(animal2);
+}
