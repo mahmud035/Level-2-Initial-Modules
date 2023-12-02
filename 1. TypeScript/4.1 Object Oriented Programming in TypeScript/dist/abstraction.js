f8f53215c7c8d36742bf39b470a91af95d660d8b
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-12-02 09:06:51 +0600

    3-9 Abstraction in OOP

--- 1. TypeScript/4.1 Object Oriented Programming in TypeScript/dist/abstraction.js
+++ 1. TypeScript/4.1 Object Oriented Programming in TypeScript/dist/abstraction.js
@@ -1,38 +1,55 @@
 "use strict";
-// interface
-class Vehicle {
-    constructor(name, brand, model) {
-        this.name = name;
-        this.brand = brand;
-        this.model = model;
+{
+    // real implementation of those methods
+    class Car {
+        constructor(name, brand, model) {
+            this.name = name;
+            this.brand = brand;
+            this.model = model;
+        }
+        startEngine() {
+            console.log(`I am starting engine`);
+        }
+        stopEngine() {
+            console.log(`I am stopping engine`);
+        }
+        move() {
+            console.log(`I am moving engine`);
+        }
+        test() {
+            console.log(`I am for testing purpose`);
+        }
     }
-    startEngine() {
-        console.log(`I am starting engine`);
+    //* Create 'instance / object Using Car Class
+    const car1 = new Car('Car Name', 'Toyota', 2000);
+    console.log(car1);
+    //* =================>>==================>>==================>>
+    //* 02. Abstraction with "abstract class"
+    /**
+     * NOTE:
+     * abstract keyword ব্যবহার করে Class তৈরি করলে, সেই Class থেকে সরাসরি 'instance / object' তৈরি করা যায় না।
+     *
+     * abstract keyword ব্যবহার করে তৈরি করা Class এর মধ্যে methods গুলো শুধু define করবো। Methods এর ভিতরে details কিছু লিখবো না, অর্থাৎ, real implementation করবো না।
+     *
+     * real implementation করবো যেখানে করা দরকার হবে সেখানে।
+     *
+     * তবে abstract class কে extends করে নতুন Class তৈরি করা যায়।
+     *
+     * সেই নতুন Class এর ভিতরে abstract class এর abstract করা methods গুলোকে অবশ্যই define করতে হবে।
+     */
+    // give idea about the methods
+    class Car2 {
     }
-    stopEngine() {
-        console.log(`I am stopping engine`);
-    }
-    move() {
-        console.log(`I am moving engine`);
-    }
-    test() {
-        console.log(`I am for testing purpose`);
-    }
-}
-const vehicle1 = new Vehicle('Car', 'Toyota', 2000);
-//* Abstraction with "abstract class"
-class Vehicle2 {
-    constructor(name, brand, model) {
-        this.name = name;
-        this.brand = brand;
-        this.model = model;
-    }
-}
-class Car extends Vehicle2 {
-    startEngine() {
-        console.log(`I am starting Engine`);
-    }
-    stopEngine() {
-        console.log(`I am stopping engine`);
+    // real implementation of those methods
+    class ToyotaCar extends Car2 {
+        startEngine() {
+            console.log(`I am starting engine`);
+        }
+        stopEngine() {
+            console.log(`I am stopping engine`);
+        }
+        move() {
+            console.log(`I am moving engine`);
+        }
     }
 }
